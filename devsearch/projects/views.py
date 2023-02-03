a752cd5eac3d2b451ec11680cc0fa8ec61ba9859
larilariv <lv23@nyu.edu>
2023-02-03 12:08:44 -0500

    Templates & Template Inheritance video complete

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def projects(request):
-    return HttpResponse('Here are our projects')
+    return render(request, 'projects/projects.html')
+
 
 def project(request, pk):
-    return HttpResponse('Project' + ' ' + str(pk))
+    return render(request, 'projects/single-project.html')
