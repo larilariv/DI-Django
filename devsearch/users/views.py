b15df5a431a30fbfff210de5893426efe9f3b7bf
larilariv <lv23@nyu.edu>
2023-02-22 21:37:02 -0500

    User Registration video complete

--- devsearch/users/views.py
+++ devsearch/users/views.py
@@ -4,9 +4,11 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.models import User
 from .models import Profile
+from .forms import CustomUserCreationForm
 # Create your views here.
 
 def loginUser(request):
+    page = 'login'
 
     if request.user.is_authenticated:
         return redirect('profiles')
@@ -30,11 +32,35 @@ def loginUser(request):
 
     return render(request, 'users/login_register.html')
 
+
 def logoutUser(request):
     logout(request)
     messages.error(request, 'User was logged out')
     return redirect('login')
 
+
+def registerUser(request):
+    page = 'register'
+    form = CustomUserCreationForm()
+
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+
+            messages.success(request, "User account created!")
+
+            login(request, user)
+            return redirect('profiles')
+        
+        else:
+            messages.error(request, "An error has occured during registration")
+
+    context = {'page': page, 'form': form}
+    return render(request, 'users/login_register.html', context)
+    
 
 def profiles(request):
     profiles = Profile.objects.all()
